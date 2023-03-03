074bdee9f8f528c94fc7c60bda66d071580aa81e
emily9949 <71063214+emily9949@users.noreply.github.com>
2023-03-04 01:48:35 +0900

    Update focusdata.py

--- focusdata.py
+++ focusdata.py
@@ -11,10 +11,9 @@ conn = pymysql.connect(
     cursorclass=pymysql.cursors.DictCursor)
 
 
-l = 0
+
 sceneTime = [0, 4, 14, 20, 22, 30] #장면 시작하는 시간을 저장하는 리스트
 focusScene = [] #모든 리뷰어의 장면별 시간, 해당 집중도를 저장하는 리스트
-emotionSceneAll = [] #모든 리뷰어의 장면별 시간, 해당 장면에서 느낀 감정, 감정 비율을 저장하는 리스트
 
 sql = "SELECT * FROM testdb.watchinginfo"
 
@@ -24,6 +23,7 @@ with conn:
         cur.execute(sql)
         output = cur.fetchall() #딕셔너리로 저장
 
+        l = 0
         while l < len(output):
             result = json.loads(output[l]['watchingInfo']) #json 데이터를 다시 한 번 딕셔너리로 변환
                 
@@ -41,9 +41,6 @@ with conn:
                             if(result['watchingInfos'][i]['gazeInfo']['eyeMovementState'] == "FIXATION" and result['watchingInfos'][i]['gazeInfo']['screenState'] == "INSIDE_OF_SCREEN"):
                                 focusTime += 1
 
-                            if((float)(result['watchingInfos'][i]['emotionInfo']['confidencePercentage'])> 40 or result['watchingInfos'][i]['emotionInfo']['confidencePercentage'] == 0):
-                                emotionList.append(result['watchingInfos'][i]['emotionInfo']['classification'])
-
 
                             i += 1
             
@@ -57,13 +54,7 @@ with conn:
                         focuslist.append(sceneTime[c+1]) #장면 구간 끝나는 시간 
                         focuslist.append(focusTime/(sceneTime[c+1]-sceneTime[c])) #해당 장면 집중률
                         focusScene.append(focuslist)
-                        
-                        emotionSceneOne = [] #리뷰어 한 명의 데이터를 저장하는 리스트
-                        emotionSceneOne.append(l+1)
-                        emotionSceneOne.append(sceneTime[c]) #장면  시작 시간 
-                        emotionSceneOne.append(sceneTime[c+1]) #장면 구간 끝나는 시간 
-                        emotionSceneOne.append(emotionList)
-                        emotionSceneAll.append(emotionSceneOne)          
+                                 
 
 
                         c += 1
@@ -91,63 +82,11 @@ with conn:
                 focus_result.append([key[0], key[1], avg])
 
 
-
-        target_list = ['neutral', 'angry', 'happy', 'disgust', 'fear', 'sad', 'surprise', 'none']
-
-
-        emotion_result_list = []
-
-
-        #전체 결과에서 가장 많이 감지된 감정 단어와, 해당 감정을 느낀 감정률 (리뷰어 한명 당) 을 리스트로 저장
-        for lst in emotionSceneAll:
-            count_dict = {t: lst[3].count(t) for t in target_list} 
-            max_count = max(count_dict.values())
-            max_word = [t for t, c in count_dict.items() if c == max_count][0]
-            percentage = max_count / len(lst[3])
-            emotion_result_list.append([lst[0], lst[1], lst[2], [max_word, percentage]])
-
-
-
-        dict_lst2 = {}
-
-        #모든 리뷰어들의 결과 값을 해당 장면 구간 별로 정렬해 딕셔너리로 저장
-        for sub_lst in emotion_result_list:
-            key = (sub_lst[1], sub_lst[2])
-            if key in dict_lst2:
-                dict_lst2[key].append(sub_lst[3])
-            else:
-                dict_lst2[key] = [sub_lst[3]]
-
-
-
-        emotion_result = [] #장면별 제일 많이 느껴진 감정, 감정률을 저장하는 최종 결과값 리스트
-           
-
-        #해당 장면 구간에서 가장 많이 느껴진 감정, 감정률을 계산하여 저장
-        for start, end in dict_lst2.keys():
-            words = []
-            probs = []
-            for word, prob in dict_lst2[(start, end)]:
-                words.append(word)
-                probs.append(prob)
-            
-            max_word = max(words, key=words.count)
-            max_word_count = words.count(max_word)
-            prob_sum = sum([prob for word, prob in dict_lst2[(start, end)] if word == max_word])
-            avg = prob_sum / max_word_count
-            
-            emotion_result.append((start, end, max_word, avg))
-
-
-        
-        #DB에 결과값 INSERT
         for res in focus_result:
             cur.execute("INSERT INTO focus (focusStartTime, focusEndTime, focusRate) VALUES (%s, %s, %s)", (res[0], res[1], res[2]))
-
-        for res in emotion_result:
-            cur.execute("INSERT INTO emotion (emotionStartTime, emotionEndTime, emotion, emotionRate) VALUES (%s, %s, %s, %s)", (res[0], res[1], res[2], res[3]))
-           
             
+
+
     conn.commit()
 
-    print("record inserted.")
+    print("focus record inserted.")
